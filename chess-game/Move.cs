735ac2de3c198b0ba385ce0d082b608047717fde
Dimi15 <spam.dimii@gmail.com>
2025-05-08 11:47:06 +0200

    Finished move function, and started the function to check if a move is legal

--- chess-game/Move.cs
+++ chess-game/Move.cs
@@ -8,9 +8,32 @@ namespace chess_game
 {
     public partial class Program
     {
-        public static void Move()
+        /// <summary>
+        /// Move a piece from Start square to End square
+        /// </summary>
+        public static void Move(int startX, int startY, int endX, int endY)
         {
-            Console.WriteLine("Legal Move");
+            board[endX, endY] = board[startX, startY];
+            board[startX, startY] = _;
+        }
+
+        /// <summary>
+        /// Given a move checks its legality
+        /// </summary>
+        /// <returns>If the move is legal</returns>
+        static bool LegalMove(int startX, int startY, int endX, int endY)
+        {
+            // Checks if it is inside the matrix
+            if (startX < 0 || startX > 7 || startY < 0 || startY > 7)
+                return false;
+
+            if (endX < 0 || endX > 7 || endY < 0 || endY > 7)
+                return false;
+
+
+            // Checks if the move is legal based on the type of piece
+
+            // PAWN:
         }
     }
 }
