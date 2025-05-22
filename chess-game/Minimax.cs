45f86ec2d5926a7e8c2dc6f346d617211abcc459
Dmytriy Kovalyshyn <spam.dimii@gmail.com>
2025-05-22 11:49:57 +0200

    Started AI algorithm

--- chess-game/Minimax.cs
+++ chess-game/Minimax.cs
@@ -8,9 +8,47 @@ namespace chess_game
 {
     public partial class Program
     {
-        public static void Minimax()
+        // Returns the best move for the computer
+        public static double Minimax(int depth, int bestStartX, int bestStartY, int bestEndX, int bestEndY)
         {
-            Console.WriteLine("Minimax");
+            double bestEvaluation = -1000;
+            double currentEvaluation = 0;
+
+            if (depth = 0)
+                return Program.Evaluation();
+
+            // Cycle through all moves for each piece in the matrix
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    // Skips empty squares and white pieces
+                    if (board[i,j] != _ && board[i,j] < BP)
+                    {
+                        for (int k = 0; k < 8; k++)
+                        {
+                            for (int l = 0; l < 8; l++)
+                            {
+                                if (Program.Move(j, i, l, k))
+                                {
+                                    currentEvaluation = Minimax(depth - 1);
+                                    // Checks if the current evaluation is greater than the best one
+                                    if (currentEvaluation > bestEvaluation)
+                                    {
+                                        bestEvaluation = currentEvaluation;
+                                        bestStartX = j;
+                                        bestStartY = i;
+                                        bestEndX = l;
+                                        bestEndY = k;
+                                    }
+                                }
+                                    
+                            }
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
